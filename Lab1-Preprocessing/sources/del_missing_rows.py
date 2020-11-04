f69481c654a9793104d16b263a4f954f418fea5b
Iris Stream <ngodaisonn@gmail.com>
2020-11-04 16:22:29 +0700

    Add task 3: function 1 2 3 4

---
+++ Lab1-Preprocessing/sources/del_missing_rows.py
@@ -0,0 +1,50 @@
+import sys, getopt
+import numpy as np
+import pandas as pd
+import argparse
+
+def arg_parser():
+    CLI = argparse.ArgumentParser()
+    CLI.add_argument(
+        "-i",
+        "--input"
+    )
+    CLI.add_argument(
+        "-m",
+        "--missingrate",
+    )
+    CLI.add_argument(
+        "-o",
+        "--output",
+    )
+    args = CLI.parse_args()
+    return args.input, args.missingrate, args.output
+
+
+if __name__ == '__main__':
+    """
+        Argument from command line. To run: 'python del_missing_rows.py --input input_file_name.csv -missing-rate 0.x -output output_file_name.csv'
+        Where input_file_name is the name of input data file
+              0.x is a number 0 <= 0.x <= 1 that we will remove all row have missing-rate >= 0.x
+              output_file_name is the name of output data file
+    """
+    
+    inputfile, missing_rate, outputfile = arg_parser()
+    missing_rate = float(missing_rate)
+    data = pd.read_csv(inputfile)
+    attributes = list(data)
+    f = open(outputfile, 'w')
+    f.write(','.join(str(attr) for attr in attributes) + '\n')
+    for index, row in data.iterrows():
+        count = 0
+        new_line =[]
+        for attr in attributes:
+            if pd.isna(row[attr]):
+                count += 1
+                new_line.append('')
+            else:
+                new_line.append(str(row[attr]))
+        if count/len(attributes) < missing_rate:
+            f.write(','.join(str(x) for x in new_line) + '\n')
+    
+    f.close()
---
+++ Lab1-Preprocessing/sources/impute.py
@@ -0,0 +1,99 @@
+import sys, getopt
+import numpy as np
+import pandas as pd
+import argparse
+
+def compute_mean(data, col):
+    print('compute mean')
+    count_instances = 0
+    sum_instances = 0.0
+    for cell in data[col]:
+        if(pd.isna(cell)):
+            continue
+        count_instances += 1
+        sum_instances += cell
+    return sum_instances / count_instances
+
+def compute_median(data, col):
+    print('compute median')
+    pool = []
+    for cell in data[col]:
+        if(pd.isna(cell)):
+            continue
+        pool.append(cell)
+    pool_len = len(pool)
+    pool.sort()
+    if(pool_len % 2 == 0):
+        return (pool[(pool_len - 1) // 2] + pool[pool_len // 2]) / 2
+    else:
+        return pool[pool_len // 2]
+
+def compute_mode(data, col):
+    print('compute mode')
+    hash_table = dict()
+    for cell in data[col]:
+        if(pd.isna(cell)):
+            continue
+        if(cell not in hash_table):
+            hash_table[cell] = 1
+        else:
+            hash_table[cell] += 1
+    mode = None
+    for key in hash_table:
+        if mode == None or hash_table[mode] < hash_table[key]:
+            mode = key
+    return mode
+
+def arg_parser():
+    CLI = argparse.ArgumentParser()
+    CLI.add_argument(
+        "-i",
+        "--input"
+    )
+    CLI.add_argument(
+        "-m",
+        "--method",
+    )
+    CLI.add_argument(
+        "-c",
+        "--columns",
+        nargs="*"
+    )
+    CLI.add_argument(
+        "-o",
+        "--output",
+    )
+    args = CLI.parse_args()
+    return args.input, args.method, args.columns, args.output
+
+
+if __name__ == '__main__':
+    """
+        Argument from command line. To run: 'python impute.py --input input_file_name.csv --method=method_name --columes col1 col2 ... --output output_file_name.csv'
+        Where input_file_name is the name of input data file
+              method_name must in ['mean', 'median', 'mode']
+              col1 col2 ... is the label of the column in data file
+              output_file_name is the name of output data file
+    """
+
+    inputfile, method, columns, outputfile = arg_parser()
+#   inputfile = 'house-prices.csv'
+#   method = 'mode'
+#   columns =['Alley']
+#   outputfile = 'hihi-prices.csv'
+
+    data = pd.read_csv(inputfile)
+    md = None
+    if method == 'mean':
+        md = compute_mean
+    elif method == 'median':
+        md = compute_median
+    elif method == 'mode':
+        md = compute_mode
+
+    for col in columns:
+        value = md(data, col)
+        for index, row in data.iterrows():
+            if(pd.isna(row[col])):
+                data.at[index, col] = value
+    data.to_csv(outputfile)
